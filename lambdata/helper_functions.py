aa42c579c6f68dcd5f53897418d1b064e9a2ba06
ivan-mihailov <ivan.i.mihailov@gmail.com>
2021-07-07 00:53:02 +0200

    Refactor Helper_Functions to include 1 Class and Code_Review Assignment.

--- lambdata/helper_functions.py
+++ lambdata/helper_functions.py
@@ -1,21 +1,28 @@
-def null_count(df):
+class CleaningData:
+    """
+    Class containing methods for identifying number of NaN values 
+    and splitting date entries in separate columns for month, day, and year.
+    """
+    def __init__(self):
+        return
+        
+    def null_count(self, df):
+        """ Check dataframe for NaNs and return the number of missing values. """
     
-    """Check a dataframe for nulls and return the number of missing 
-    values."""
-
-    x, y = df.shape
-    all_count = x*y
-    non_null_count = df.count().sum()
+        x, y = df.shape
+        all_count = x*y
+        non_null_count = df.count().sum()
     
-    return all_count - non_null_count
+        return all_count - non_null_count
 
-def split_dates(date_series):
+    def split_dates(self, date_series):
+        """
+        Method to split dates of format "MM/DD/YYYY" into multiple columns 
+        (df['month'], df['day'], df['year']) and then return the 
+        same dataframe with those additional columns.
+        """
 
-    """Function to split dates of format "MM/DD/YYYY" into 
-    multiple columns (df['month'], df['day'], df['year']) 
-    and then return the same dataframe with those additional columns."""
+        df = date_series.str.split(pat='/', expand=True)
+        df = df.rename(columns={0:'month', 1:'day', 2:'year'})
 
-    df = date_series.str.split(pat='/', expand=True)
-    df = df.rename(columns={0:'month', 1:'day', 2:'year'})
-
-    return df
+        return df
